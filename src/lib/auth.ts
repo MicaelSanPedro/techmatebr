75152d1dc49e1effb6be02ff345c79fcbf2fdf9d
MicaelSanPedro <167715159+MicaelSanPedro@users.noreply.github.com>
2026-06-05 04:21:14 +0000

    Simplify auth config to debug Configuration error

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -7,32 +7,10 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     GitHub({
       clientId: process.env.AUTH_GITHUB_ID,
       clientSecret: process.env.AUTH_GITHUB_SECRET,
-      authorization: {
-        params: { scope: "read:user user:email gist" },
-      },
     }),
     Google({
       clientId: process.env.AUTH_GOOGLE_ID,
       clientSecret: process.env.AUTH_GOOGLE_SECRET,
     }),
   ],
-  callbacks: {
-    async jwt({ token, account, profile }) {
-      if (account) {
-        token.accessToken = account.access_token;
-      }
-      if (profile) {
-        token.login = (profile as any).login;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      session.user.accessToken = token.accessToken as string;
-      session.user.login = token.login as string;
-      return session;
-    },
-  },
-  pages: {
-    signIn: undefined,
-  },
 });
